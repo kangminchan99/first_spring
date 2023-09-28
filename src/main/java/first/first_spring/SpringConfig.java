7c5afab0ab67b0e080dfc3a744f8a27122ded4a7
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2023-09-28 23:33:43 +0900

    스프링 설정 변경

--- src/main/java/first/first_spring/SpringConfig.java
+++ src/main/java/first/first_spring/SpringConfig.java
@@ -1,14 +1,25 @@
 package first.first_spring;
 
 
+import first.first_spring.repository.JdbcMemberRepository;
 import first.first_spring.repository.MemberRepository;
-import first.first_spring.repository.MemoryMemberRepository;
 import first.first_spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository());
@@ -16,6 +27,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
